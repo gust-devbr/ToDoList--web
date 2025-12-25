7ef92b816fa55b19ff9dcf64539cdc2e7ac69e3e
Gustavo Oliv <calacogustavo@gmail.com>
2025-12-25 15:27:04 -0300

    Adicionada função de renomear tarefas e adição de tratamento de erros(Try/Catch)

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,14 +1,13 @@
 import express from 'express'
 import cors from 'cors'
-
-import routesTasks from './routes/tasks.routes.js'
+import tasksRoutes from './routes/tasks.routes.js'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use('/tasks', routesTasks)
+app.use('/tasks', tasksRoutes)
 
-app.listen(3003, () => {
-    console.log('Rodando em http://localhost:3003')
-});
+app.listen(3333, () => {
+    console.log('Rodando em http://localhost:3333')
+})
