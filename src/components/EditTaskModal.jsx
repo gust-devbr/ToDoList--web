933e78e9906fc23e7110fef2d0937fa5055f09a5
Gustavo Oliv <calacogustavo@gmail.com>
2026-01-18 19:05:33 -0300

    fix: correção na posição dos Modais e aplicação do tema nos mesmos

--- src/components/EditTaskModal.jsx
+++ src/components/EditTaskModal.jsx
@@ -1,8 +1,10 @@
 import { FaSave } from 'react-icons/fa';
 import { MdCancel } from 'react-icons/md';
 import { useEffect } from 'react';
+import { useTheme } from '../context/ThemeContext';
 
 export default function EditTaskModal({ isOpen, onClose, onRename, value, setValue }) {
+    const { theme } = useTheme();
 
     useEffect(() => {
         function handleKeyDown(e) {
@@ -26,8 +28,8 @@ export default function EditTaskModal({ isOpen, onClose, onRename, value, setVal
     if (!isOpen) return null;
 
     return (
-        <div className='modal-overlay'>
-            <div className='modal'>
+        <div style={{ ...styles.modalOverlay }}>
+            <div style={{ ...styles.modal, background: theme.background }}>
                 <h2>Editar Tarefa</h2>
 
                 <input
@@ -37,11 +39,43 @@ export default function EditTaskModal({ isOpen, onClose, onRename, value, setVal
                     onChange={(e) => setValue(e.target.value)}
                 />
 
-                <div className='modal-actions'>
-                    <button title='Salvar' onClick={onRename}> <FaSave /> </button>
-                    <button title='Cancelar' onClick={onClose}> <MdCancel /> </button>
+                <div style={{ ...styles.modalActions }}>
+                    <button style={{ ...styles.button, color: theme.text }} title='Salvar' onClick={onRename}> <FaSave /> </button>
+                    <button style={{ ...styles.button, color: theme.text }} title='Cancelar' onClick={onClose}> <MdCancel /> </button>
                 </div>
             </div>
         </div>
     )
+};
+
+const styles = {
+    modalOverlay: {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        background: 'rgba(0, 0, 0, 0.5)',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    modal: {
+        padding: '20px',
+        borderRadius: '8px',
+        width: '250px',
+        height: '100px',
+    },
+    modalActions: {
+        display: 'flex',
+        justifyContent: 'flex-end',
+        marginTop: '-22px',
+        gap: '5px',
+    },
+    button: {
+        border: 'none',
+        background: 'transparent',
+        fontSize: '16px',
+        cursor: 'pointer',
+    },
 };
