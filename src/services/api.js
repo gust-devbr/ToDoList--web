c86efcdb98c1a51b25cd45ef0f02248f30ebcb29
Gustavo Oliv <calacogustavo@gmail.com>
2026-02-11 14:36:18 -0300

    chore: alterar URL da API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://backend-todolist-nygg.onrender.com'
+    baseURL: 'https://backend-todolist-production.up.railway.app'
 });
 
 export default api;
