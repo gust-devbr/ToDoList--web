db46492eae2c98b208f1514e0a035856220ed851
Gustavo Oliv <calacogustavo@gmail.com>
2026-03-02 10:20:04 -0300

    fix: parâmetros de caminho nos endpoints PATCH, PUT e DELETE

--- src/app/api/private/notes/[id]/route.js
+++ src/app/api/private/notes/[id]/route.js
@@ -2,9 +2,9 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { getUserFromToken } from "@/lib/auth";
 
-export async function DELETE(context) {
+export async function DELETE(req, { params }) {
     try {
-        const { id } = await context.params;
+        const { id } = await params;
 
         const user = await getUserFromToken();
         if (!user) return NextResponse.json({ error: "Não autorizado" }, { status: 403 });
@@ -25,9 +25,9 @@ export async function DELETE(context) {
     }
 };
 
-export async function PUT(req, context) {
+export async function PUT(req, { params }) {
     try {
-        const { id } = await context.params;
+        const { id } = await params;
 
         const { title, content } = await req.json();
         if (!title || !content) return NextResponse.json({ error: "Dados incompletoso" }, { status: 400 });
@@ -52,9 +52,9 @@ export async function PUT(req, context) {
     }
 };
 
-export async function PATCH(req, context) {
+export async function PATCH(req, { params }) {
     try {
-        const { id } = await context.params;
+        const { id } = await params;
 
         const user = await getUserFromToken();
         if (!user) return NextResponse.json({ error: "Não autorizado" }, { status: 403 });
