26212665c21425f288f9270bfc1671be4d0bcd00
Gustavo Oliv <calacogustavo@gmail.com>
2026-03-06 22:21:37 -0300

    refactor: unificar modais de criar e editar

--- src/components/index.js
+++ src/components/index.js
@@ -7,10 +7,8 @@ export { AppSidebar } from './layout/AppSidebar';
 
 // Modals
 export { ChangePassModal } from './modals/ChangePassModal';
-export { ContactModal } from './modals/ContactModal';
-export { NoteModal } from './modals/NoteModal';
-export { TaskModal } from './modals/TaskModal';
 export { ConfirmDialog } from './modals/ConfirmDialog';
+export { ItemModal } from './modals/ItemModal';
 
 // Tabelas específicas
 export { TableContact } from './tables/TableContact';
@@ -23,4 +21,5 @@ export { StatusPieChart } from './charts/StatusPieChart';
 //Utils
 export { filterConfig } from './utils/filters-config';
 export { chartConfig } from './utils/chart-config';
+export { modalConfig } from './utils/modal-config';
 export * from './utils/validator';
