cb79365bd09852d0d131d72e890b1dc38f2d06e2
Gustavo Oliv <calacogustavo@gmail.com>
2026-04-30 23:09:28 -0300

    feat(archived): criar página para tarefas arquivadas

---
+++ src/app/api/private/tasks/[id]/edit/route.ts
@@ -0,0 +1,25 @@
+import { taskService } from "@/services/taskService"
+import { getToken } from "@/utils/auth"
+import { Response } from "@/utils/response"
+import { NextRequest } from "next/server"
+
+export async function PUT(
+    req: NextRequest,
+    { params }: { params: Promise<{ id: string }> }
+) {
+    try {
+        const { title } = await req.json()
+
+        const { id } = await params
+        if (!id) return Response.error("ID não fornecido", null, 400)
+
+        const user = await getToken(req)
+        if (!user) return Response.error("Não autorizado", null, 401)
+
+        await taskService.update(id, title)
+
+        return Response.success(null, "Tarefa editada")
+    } catch (error) {
+        return Response.error("Erro ao editar tarefa", error)
+    }
+}
--- src/utils/searchParams.ts
+++ src/utils/searchParams.ts
@@ -1,16 +1,19 @@
 import { NextRequest } from "next/server";
 
-type Status = "all" | "completed" | "pending"
+type Status = "all" | "completed" | "pending" | "archived"
 
 export function getSearchParams(req: NextRequest) {
     const url = new URL(req.url)
 
     const rawStatus = url.searchParams.get("status")
+    const archivedParam: string | null = url.searchParams.get("archived")
 
     const status: Status =
-        rawStatus === "completed" || rawStatus === "pending"
+        rawStatus === "completed" ||
+            rawStatus === "pending" ||
+            rawStatus === "archived"
             ? rawStatus
             : "all"
 
-    return { status }
+    return { status, archivedParam }
 }
